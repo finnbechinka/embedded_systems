60235261339eb4f6c7ebd208947157f3956ec68d
Finn <contact@bechinka.eu>
2024-01-25 06:53:57 +0100

    add p4

---
+++ praktikum_4/src/pong.c
@@ -0,0 +1,20 @@
+/**
+ * @file xmas.c
+ * @author Finn Bechinka (finn.bechinka@hsbi.de)
+ * @brief xmas stuff.
+ * @version 0
+ * @date 20.12.2023
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "startup.h"
+#include "wrap.h"
+
+int main(void)
+{
+
+	return 0;
+}
